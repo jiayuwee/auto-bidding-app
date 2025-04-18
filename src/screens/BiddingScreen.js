4bda6e67f9908ba97900193c7a66248412ee2ed4
jiayuwee <992956705@qq.com>
2025-04-18 20:39:50 +0800

    modified BiddingScreen.js modified package.json modified package-lock.json modified src/index.js modified src/screens/BiddingScreen.js modified src/screens/LoginScreen.js modified src/screens/import React from'react';.js modified src/services/ProjectService.js modified yarn.lock

---
+++ src/screens/BiddingScreen.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = require("react");
+var react_native_1 = require("react-native");
+;
+var BiddingScreen = function () {
+    return (<react_native_1.View>
+      {/* 屏幕内容 */}
+    </react_native_1.View>);
+};
+exports.default = BiddingScreen;
---
+++ src/screens/import React from 'react';.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_native_1 = require("react-native");
+var stack_1 = require("@react-navigation/stack");
+var Stack = (0, stack_1.createStackNavigator)();
+var App = function () {
+    return style = { styles: styles, : .container } >
+        barStyle;
+    "dark-content" /  >
+        initialRouteName;
+    "Login" >
+        name;
+    "Login";
+    component = { require: function () { }, : .default } /  >
+        name;
+    "Home";
+    component = { require: function () { }, : .default } /  >
+        name;
+    "Bidding";
+    component = { require: function () { }, : .default } /  >
+        /Stack.Navigator>
+        < /NavigationContainer>
+        < /SafeAreaView>;
+};
+;
+;
+var styles = react_native_1.StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+    },
+});
+exports.default = App;
